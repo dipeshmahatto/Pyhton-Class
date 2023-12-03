6fd467db2fe63edcc18979ec3f3755542b9f0357
dipesh <dipeshmahatto@gmail.com>
2023-12-03 11:13:36 +0545

    day 2

---
+++ day2/hello.py
@@ -0,0 +1,14 @@
+# print("Hello World")
+# print("Welcome")
+
+a = 5 ; 
+print("The number is :",a)
+
+# f is used to format 
+# print(f"The number is {a}")
+
+name = input("Enter your name ")
+print("Your name is :",name)
+
+age = input("Enter your age ")
+print("Your age is ",age)
---
+++ day2/type.py
@@ -0,0 +1,13 @@
+a = 5
+b= "10"
+name = "Dipesh"
+
+# TYPE is use to know the variable datatype 
+print(type(a))
+print(type(b))
+print(type(name))
+
+# ID is use to know the location of variable in memory 
+print(id(a))
+print(id(b))
+print(id(name))
