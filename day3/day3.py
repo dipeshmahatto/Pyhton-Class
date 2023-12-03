9ea94da6f1b5f20cf88a4634b5b22e703f5bffc1
dipesh <dipeshmahatto@gmail.com>
2023-12-03 12:11:58 +0545

    day3

---
+++ day3/day3.py
@@ -0,0 +1,29 @@
+num_1=5
+
+print(num_1)
+
+
+
+# a = input ("Ente your name ")
+# print(a)
+
+
+#  Variable naming convention
+
+# 1) variable_name only contains number , underscore  characters.
+# 2) Special character not allowed allowed.
+# 3) Space not allowed.
+# 4) Can't start with numbers.      
+
+'''Builtin python datatype are :
+
+1) int
+2) float 
+3) String
+4) List---> l = [45,75,84,"Dipesh"] -----------> can be editable
+5) Tuple --> t= (10,20,"Mahato)---->can not  be editable
+6) Dictionary ---> d= {"name":"ABC","email":"abc@gmail.com"}-----> key:value -----> can be editable
+7) Set-----> s = {10,20,54} -----------> can be editable
+8) Boolen --->ture or false
+9) Complex ---> c = 4+3j '''
+
