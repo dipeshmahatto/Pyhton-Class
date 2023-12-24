b81a7a0bf6810d4772080b6df6285b37390c12fb
dipesh <dipeshmahatto@gmail.com>
2023-12-24 12:08:14 +0545

    day10

--- day10/function.py
+++ day10/function.py
@@ -5,14 +5,17 @@
 # print(result)
 
 
-def SI(p,r,t):
-    return (p*t*r)/100
+# def SI(p,r,t):
+#     return (p*t*r)/100
 
-p = int(input("Enter the principle amount: "))
-r = int(input("Enter the rate: "))
-t = int(input("Enter the time in months: "))
-SimpleInterst = SI(p,r,t)
-print(SimpleInterst)
+# p = int(input("Enter the principle amount: "))
+# r = int(input("Enter the rate: "))
+# t = int(input("Enter the time in months: "))
+# SimpleInterst = SI(p,r,t)
+# print(SimpleInterst)
 
 
+def person(name, age:int):
+    print("print name is ",name," and age  is ",age)
 
+person("dipesh",20)
